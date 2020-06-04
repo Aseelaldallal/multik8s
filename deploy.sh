45d85d809f66f22a8967274d8fe081c1f82930a1
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-06-03 20:10:08 -0400

    remove comments

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t aseel/multi-client:latest -t aseel/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t aseel/multi-server:latest -t aseel/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t aseel/multi-worker:latest -t aseel/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-# Take images and push them to docker hub (we already logged into docker in travis.yaml)
-docker push aseel/multi-client:latest
-docker push aseel/multi-server:latest
-docker push aseel/multi-worker:latest
-docker push aseel/multi-client:$SHA
-docker push aseel/multi-server:$SHA
-docker push aseel/multi-worker:$SHA
-# Apply all configs in k8s directory (we already configured google cloud kubectl)
+docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push stephengrider/multi-client:latest
+docker push stephengrider/multi-server:latest
+docker push stephengrider/multi-worker:latest
+
+docker push stephengrider/multi-client:$SHA
+docker push stephengrider/multi-server:$SHA
+docker push stephengrider/multi-worker:$SHA
+
 kubectl apply -f k8s
-# Imperatively set latest images on each deployment
-kubectl set image deployments/server-deployment server=aseel/multi-server:$SHA
-kubectl set image deployments/client-deployment client=aseel/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=aseel/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
